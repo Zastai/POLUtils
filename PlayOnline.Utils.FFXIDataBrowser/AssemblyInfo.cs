f594c0c78a8b992192c637cf11d77b41e11c32c1
Tim Van Holder <Zastai@users.noreply.github.com>
2005-01-17 19:52:09 +0000

    New file.  Assembly attributes.

---
+++ PlayOnline.Utils.FFXIDataBrowser/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyTitle        ("PlayOnline.Utils.FFXIDataBrowser")]
+[assembly: AssemblyDescription  ("Components for browsing Final Fantasy XI data files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany      ("")]
+[assembly: AssemblyProduct      ("POLUtils")]
+[assembly: AssemblyCopyright    ("Copyright 2004,2005 Tim Van Holder")]
+[assembly: AssemblyTrademark    ("PlayOnline, Final Fantasy and Vanadiel are registered trademarks of Square Enix Co., Ltd.")]
+[assembly: AssemblyCulture      ("")]		
+
+[assembly: AssemblyVersion      ("0.5.0.0")]
+
+[assembly: AssemblyDelaySign    (false)]
+[assembly: AssemblyKeyFile      (@"..\..\..\POLUtils.snk")]
+[assembly: AssemblyKeyName      ("")]
