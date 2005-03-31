d9adc5bf4b14584fd8c527db30524536a0612655
Tim Van Holder <Zastai@users.noreply.github.com>
2005-03-31 17:56:00 +0000

    Added version of GetText() that takes an Assembly to get the text from (needed for NamedEnum).

--- PlayOnline.Core/I18N.cs
+++ PlayOnline.Core/I18N.cs
@@ -8,8 +8,14 @@ namespace PlayOnline.Core {
   public class I18N {
 
     public static string GetText(string Name) {
-    ResourceManager Resources = new ResourceManager("Messages", Assembly.GetCallingAssembly());
+      return I18N.GetText(Name, Assembly.GetCallingAssembly());
+    }
+
+    public static string GetText(string Name, Assembly A) {
+    ResourceManager Resources = new ResourceManager("Messages", A);
     string ResourceString = Resources.GetObject(Name, CultureInfo.CurrentUICulture) as string;
+      if (ResourceString == null)
+	ResourceString = Resources.GetObject(Name, CultureInfo.InvariantCulture) as string;
       if (ResourceString == null)
 	return Name;
       else
