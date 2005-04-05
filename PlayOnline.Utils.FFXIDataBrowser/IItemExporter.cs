b97d5799a7f3d63485b2b921cde796f54b55bda0
Tim Van Holder <Zastai@users.noreply.github.com>
2005-04-05 10:57:38 +0000

    Made IItemExporter an abstract class instead of an interface. Added a static property and method to handle the selection of the export output location.

--- PlayOnline.Utils.FFXIDataBrowser/IItemExporter.cs
+++ PlayOnline.Utils.FFXIDataBrowser/IItemExporter.cs
@@ -1,14 +1,55 @@
 using System;
+using System.IO;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 using PlayOnline.Core;
 using PlayOnline.FFXI;
 
 namespace PlayOnline.Utils.FFXIDataBrowser {
 
-  internal interface IItemExporter {
+  internal abstract class IItemExporter {
 
-    void DoExport(FFXIItem[] Items);
+    public abstract void DoExport(FFXIItem[] Items);
+
+    private static FolderBrowserDialog dlgBrowseFolder = null;
+
+    private static void PrepareFolderBrowser() {
+      if (IItemExporter.dlgBrowseFolder == null) {
+	IItemExporter.dlgBrowseFolder = new FolderBrowserDialog();
+	IItemExporter.dlgBrowseFolder.Description = I18N.GetText("Export:DirDialogDesc");
+      string DefaultLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), Path.Combine("POLUtils", "Exported Item Data"));
+      string InitialLocation = null;
+	try {
+	RegistryKey RK = Registry.CurrentUser.CreateSubKey(@"Software\Pebbles\POLUtils");
+	  InitialLocation = RK.GetValue("Export Location", null) as string;
+	  RK.Close();
+	} catch { }
+	if (InitialLocation == null || InitialLocation == String.Empty)
+	  InitialLocation = DefaultLocation;
+	if (!Directory.Exists(InitialLocation))
+	  Directory.CreateDirectory(InitialLocation);
+	IItemExporter.dlgBrowseFolder.SelectedPath = InitialLocation;
+      }
+    }
+
+    public static string OutputPath {
+      get {
+	IItemExporter.PrepareFolderBrowser();
+	return IItemExporter.dlgBrowseFolder.SelectedPath;
+      }
+    }
+
+    public static void BrowseForOutputPath() {
+      IItemExporter.PrepareFolderBrowser();
+      if (IItemExporter.dlgBrowseFolder.ShowDialog() == DialogResult.OK) {
+	try {
+	RegistryKey RK = Registry.CurrentUser.CreateSubKey(@"Software\Pebbles\POLUtils");
+	  RK.SetValue("Export Location", IItemExporter.dlgBrowseFolder.SelectedPath);
+	  RK.Close();
+	} catch { }
+      }
+    }
 
   }
 
