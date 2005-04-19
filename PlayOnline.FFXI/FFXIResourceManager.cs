384d08934d54e17244f3546c53fef019b4dd3ec9
Tim Van Holder <Zastai@users.noreply.github.com>
2005-04-19 17:21:30 +0000

    New file.  Provides resource string retrieval services to FFXIEncoding.

---
+++ PlayOnline.FFXI/FFXIResourceManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+
+using PlayOnline.Core;
+
+namespace PlayOnline.FFXI {
+
+  public class FFXIResourceManager {
+
+    public static void Init() {
+    }
+
+    public static string GetResourceString(uint ResourceID) {
+      return "String Resource";
+    }
+
+  }
+
+}
