13aed8bb3e5fe2230879ae6efd362880fa35bfea
Tim Van Holder <Zastai@users.noreply.github.com>
2005-04-19 17:16:02 +0000

    Use a sub-key ("Character Names") of the main config key to store character name mappings. Moved CharacterCollection here (from Collections.cs).

--- PlayOnline.FFXI/Character.cs
+++ PlayOnline.FFXI/Character.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.IO;
 using Microsoft.Win32;
 
@@ -21,14 +22,14 @@ namespace PlayOnline.FFXI {
       get {
       string DefaultName = String.Format("Unknown Character ({0})", this.ID_);
       string value = null;
-	using (RegistryKey NameMappings = POL.OpenPOLUtilsConfigKey(true)) {
+	using (RegistryKey NameMappings = POL.OpenPOLUtilsConfigKey("Character Names", true)) {
 	  if (NameMappings != null)
 	    value = NameMappings.GetValue(this.ID_, null) as string;
 	}
 	return ((value == null) ? DefaultName : value);
       }
       set {
-	using (RegistryKey NameMappings = POL.OpenPOLUtilsConfigKey(true)) {
+	using (RegistryKey NameMappings = POL.OpenPOLUtilsConfigKey("Character Names", true)) {
 	  if (NameMappings != null) {
 	    if (value == null)
 	      NameMappings.DeleteValue(this.ID_, false);
@@ -77,6 +78,30 @@ namespace PlayOnline.FFXI {
       MacroFolder MF = MacroFolder.LoadFromMacroBar(Path.Combine(this.DataDir_, String.Format("mcr{0:#}.dat", i)));
 	MF.Name = String.Format(I18N.GetText("MacroBarLabel"), i + 1);
 	this.MacroBars_.Add(MF);
+      }
+    }
+
+  }
+
+  public class CharacterCollection : ReadOnlyCollectionBase {
+
+    public void Add     (Character C) {        this.InnerList.Add     (C); }
+    public bool Contains(Character C) { return this.InnerList.Contains(C); }
+    public int  IndexOf (Character C) { return this.InnerList.IndexOf (C); }
+    public void Remove  (Character C) {        this.InnerList.Remove  (C); }
+
+    public Character this[int Index] {
+      get { return this.InnerList[Index] as Character; }
+      set { this.InnerList[Index] = value; }
+    }
+
+    public Character this[string ID] {
+      get {
+	foreach (Character C in this.InnerList) {
+	  if (C.ID == ID)
+	    return C;
+	}
+	return null;
       }
     }
 
