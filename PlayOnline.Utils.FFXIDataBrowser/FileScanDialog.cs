ac59ae32ed7e9a52503d1f9fa9673f91edb76f5a
Tim Van Holder <Zastai@users.noreply.github.com>
2005-04-23 11:17:34 +0000

    Made the use of threads a runtime choice rather than a compile-time one. Added AllowAbort and ShowProgressDetails configuration settings. Added loading of title & ability information.

--- PlayOnline.Utils.FFXIDataBrowser/FileScanDialog.cs
+++ PlayOnline.Utils.FFXIDataBrowser/FileScanDialog.cs
@@ -1,16 +1,10 @@
-// If this is defined, the scanning is done in a thread.  This has the positive effect of being able to abort
-// the scan by closing the window, but the negative effect of making the scan much slower...
-//#define ThreadBased
-
 using System;
 using System.Collections;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Text;
-#if ThreadBased
 using System.Threading;
-#endif
 using System.Windows.Forms;
 
 using PlayOnline.Core;
@@ -21,6 +15,9 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
   internal class FileScanDialog : System.Windows.Forms.Form {
 
     private string FileName;
+
+    public static bool AllowAbort = false;
+    public static bool ShowProgressDetails = false;
 
     public ArrayList StringTableEntries = new ArrayList();
     public ArrayList Images             = new ArrayList();
@@ -39,9 +36,7 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
       InitializeComponent();
       this.FileName = FileName;
       this.DialogResult = DialogResult.None;
-#if !ThreadBased
-      this.ControlBox = false;
-#endif
+      this.ControlBox = FileScanDialog.AllowAbort;
     }
 
     #region Scanners
@@ -57,28 +52,39 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
 	if (BR != null && BR.BaseStream.CanSeek) {
   	  Application.DoEvents();
 	  BR.BaseStream.Seek(0, SeekOrigin.Begin);
-	  this.ScanItemData(BR);
+	  this.ScanDialogFile(BR);
+	  if (this.StringTableEntries.Count != 0)
+	    goto Done;
   	  Application.DoEvents();
 	  BR.BaseStream.Seek(0, SeekOrigin.Begin);
-	  this.ScanDialogFile(BR);
-	  if (this.StringTableEntries.Count == 0) { // These share a result field (they're mutually exclusive anyway)
-  	    Application.DoEvents();
-	    BR.BaseStream.Seek(0, SeekOrigin.Begin);
-	    this.ScanXIStringFile(BR);
-	  }
-	  if (this.Items.Count == 0) { // Items already contain the images - don't scan for them again
-  	    Application.DoEvents();
-	    BR.BaseStream.Seek(0, SeekOrigin.Begin);
-	    this.ScanImages(BR);
-	  }
+	  this.ScanXIStringFile(BR);
+	  if (this.StringTableEntries.Count != 0)
+	    goto Done;
+  	  Application.DoEvents();
+	  BR.BaseStream.Seek(0, SeekOrigin.Begin);
+	  this.ScanSimpleStringFile(BR);
+	  if (this.StringTableEntries.Count != 0)
+	    goto Done;
+  	  Application.DoEvents();
+	  BR.BaseStream.Seek(0, SeekOrigin.Begin);
+	  this.ScanAbilityFile(BR);
+	  if (this.StringTableEntries.Count != 0)
+	    goto Done;
+  	  Application.DoEvents();
+	  BR.BaseStream.Seek(0, SeekOrigin.Begin);
+	  this.ScanItemData(BR);
+	  if (this.Items.Count != 0)
+	    goto Done;
+	  // No specific format recognized - scan for embedded image data
+  	  Application.DoEvents();
+	  BR.BaseStream.Seek(0, SeekOrigin.Begin);
+	  this.ScanImages(BR);
+	Done:
 	  BR.Close();
 	}
       }
-#if ThreadBased
-      this.ScanThread = null;
-#else
       this.Scanning = false;
-#endif
+      this.ScanThread = null;
       this.DialogResult = DialogResult.OK;
       this.Close();
     }
@@ -87,6 +93,141 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
       this.prbScanProgress.Value = (int) (Math.Min((decimal) Current / Max, 1.0M) * this.prbScanProgress.Maximum);
       Application.DoEvents();
     }
+
+    #region Abilities
+
+    private int CountBits(byte B) {
+    int Count = 0;
+      while (B != 0) {
+	if ((B & 0x01) != 0)
+	  ++Count;
+	 B >>= 1;
+      }
+      return Count;
+    }
+
+    private void DecodeAbility(byte[] Bytes) {
+    int BitCount = this.CountBits(Bytes[2]) - this.CountBits(Bytes[11]) + this.CountBits(Bytes[12]);
+    byte ShiftSize = 0;
+      switch (Math.Abs(BitCount) % 5) {
+	case 0: ShiftSize = 7; break;
+	case 1: ShiftSize = 1; break;
+	case 2: ShiftSize = 6; break;
+	case 3: ShiftSize = 2; break;
+	case 4: ShiftSize = 5; break;
+      }
+      for (int i = 0; i < Bytes.Length; ++i)
+	Bytes[i] = (byte) ((Bytes[i] >> ShiftSize) | (Bytes[i] << (8 - ShiftSize)));
+    }
+
+    private void ScanAbilityFile(BinaryReader BR) {
+      this.lblScanProgress.Text = I18N.GetText("AbilityCheck");
+      this.SetProgress(0, 1);
+      BR.BaseStream.Seek(0, SeekOrigin.Begin);
+      if ((BR.BaseStream.Length % 0x400) != 0)
+	return;
+    long EntryCount = BR.BaseStream.Length / 0x400;
+    Encoding E = new FFXIEncoding();
+      this.lblScanProgress.Text = I18N.GetText("AbilityLoad");
+      for (int i = 0; i < EntryCount; ++i) {
+	// index (ushort), unknown = 8 bytes, name = 32 bytes, desc = 96 bytes, rest is padding, last byte is 0xff
+      byte[] Bytes = BR.ReadBytes(0x400);
+	this.DecodeAbility(Bytes);
+	if (Bytes[0x3ff] != 0xff || ((Bytes[0] + (Bytes[1] << 8)) != i)) {
+	  this.StringTableEntries.Clear();
+	  return;
+	}
+	// Even better: separate results with name & desc separate
+	this.StringTableEntries.Add(String.Format("\u3010{0}\u3011 {1}", E.GetString(Bytes, 10, 32).TrimEnd('\0'), E.GetString(Bytes, 42, 96).TrimEnd('\0')));
+	if (FileScanDialog.ShowProgressDetails)
+	  this.lblScanProgress.Text = String.Format(I18N.GetText("AbilityLoadProgress"), i + 1, EntryCount);
+	this.SetProgress(i + 1, EntryCount);
+      }
+    }
+
+    #endregion
+
+    #region Dialog Files
+
+    private void ScanDialogFile(BinaryReader BR) {
+      this.lblScanProgress.Text = I18N.GetText("DialogCheck");
+      this.SetProgress(0, 1);
+      BR.BaseStream.Seek(0, SeekOrigin.Begin);
+      if (BR.BaseStream.Length < 4)
+	return;
+    uint FileSizeMaybe = BR.ReadUInt32();
+      if (FileSizeMaybe != (0x10000000 + BR.BaseStream.Length - 4))
+	return;
+    uint FirstTextPos = (BR.ReadUInt32() ^ 0x80808080);
+      if ((FirstTextPos % 4) != 0 || FirstTextPos > BR.BaseStream.Length || FirstTextPos < 8)
+	return;
+    uint EntryCount = FirstTextPos / 4;
+    Encoding E = new FFXIEncoding();
+      this.lblScanProgress.Text = I18N.GetText("DialogLoad");
+      for (int i = 0; i < EntryCount; ++i) {
+	BR.BaseStream.Seek(4 + 4 * i, SeekOrigin.Begin);
+      long Offset = (BR.ReadUInt32() ^ 0x80808080);
+      long NextOffset = (((i + 1) == EntryCount) ? BR.BaseStream.Length : (BR.ReadUInt32() ^ 0x80808080));
+	if (NextOffset < Offset || NextOffset > (Offset + 1024)) { // Sanity check - the 1024 is arbitrary
+	  this.StringTableEntries.Clear();
+	  return;
+	}
+	BR.BaseStream.Seek(4 + Offset, SeekOrigin.Begin);
+      byte[] TextBytes = BR.ReadBytes((int) (NextOffset - Offset));
+	for (int j = 0; j < TextBytes.Length; ++j)
+	  TextBytes[j] ^= 0x80;
+      string Entry = String.Empty;
+      int LastPos = 0;
+	for (int j = 0; j < TextBytes.Length; ++j) {
+	  if (TextBytes[j] == 0x07) { // Line Break
+	    if (LastPos < j)
+	      Entry += E.GetString(TextBytes, LastPos, j - LastPos);
+	    Entry += String.Format("{0}NewLine{1}", FFXIEncoding.SpecialMarkerStart, FFXIEncoding.SpecialMarkerEnd);
+	    LastPos = j + 1;
+	  }
+	  else if (TextBytes[j] == 0x08) { // Character Name (You)
+	    if (LastPos < j)
+	      Entry += E.GetString(TextBytes, LastPos, j - LastPos);
+	    Entry += String.Format("{0}Player Name{1}", FFXIEncoding.SpecialMarkerStart, FFXIEncoding.SpecialMarkerEnd);
+	    LastPos = j + 1;
+	  }
+	  else if (TextBytes[j] == 0x09) { // Character Name (They)
+	    if (LastPos < j)
+	      Entry += E.GetString(TextBytes, LastPos, j - LastPos);
+	    Entry += String.Format("{0}Speaker Name{1}", FFXIEncoding.SpecialMarkerStart, FFXIEncoding.SpecialMarkerEnd);
+	    LastPos = j + 1;
+	  }
+	  else if (TextBytes[j] == 0x0a && j + 1 < TextBytes.Length) { // ???
+	    if (LastPos < j)
+	      Entry += E.GetString(TextBytes, LastPos, j - LastPos);
+	    Entry += String.Format("{0}Parameter {1}{2}", FFXIEncoding.SpecialMarkerStart, TextBytes[j + 1], FFXIEncoding.SpecialMarkerEnd);
+	    LastPos = j + 2;
+	    ++j;
+	  }
+	  else if (TextBytes[j] == 0x0b) { // Indicates that the lines after this are in a prompt window
+	    if (LastPos < j)
+	      Entry += E.GetString(TextBytes, LastPos, j - LastPos);
+	    Entry += String.Format("{0}Selection Dialog{1}", FFXIEncoding.SpecialMarkerStart, FFXIEncoding.SpecialMarkerEnd);
+	    LastPos = j + 1;
+	  }
+	  else if (TextBytes[j] == 0x7f && j + 2 < TextBytes.Length) { // Unknown Type of Text Substitution
+	    if (LastPos < j)
+	      Entry += E.GetString(TextBytes, LastPos, j - LastPos);
+	    Entry += String.Format("{0}Marker: {1:X2}{2:X2}{3:X2}{4}", FFXIEncoding.SpecialMarkerStart, TextBytes[j + 0], TextBytes[j + 1], TextBytes[j + 2], FFXIEncoding.SpecialMarkerEnd);
+	    LastPos = j + 3;
+	    j += 2;
+	  }
+	}
+	if (LastPos < TextBytes.Length)
+	  Entry += E.GetString(TextBytes, LastPos, TextBytes.Length - LastPos);
+	this.StringTableEntries.Add(Entry);
+	if (FileScanDialog.ShowProgressDetails)
+	  this.lblScanProgress.Text = String.Format(I18N.GetText("DialogLoadProgress"), i + 1, EntryCount);
+	this.SetProgress(i + 1, EntryCount);
+      }
+    }
+
+    #endregion
 
     #region Images
 
@@ -97,20 +238,20 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
     long Pos = 0;
     int ImageCount = 0;
       this.lblScanProgress.Text = String.Format(I18N.GetText("ImageScan"), ImageCount);
-      this.prbScanProgress.Value = 0;
+      this.SetProgress(0, 1);
       BR.BaseStream.Seek(Pos, SeekOrigin.Begin);
       while (Pos < MaxPos) {
       FFXIGraphic FG = FFXIGraphic.Read(BR);
 	if (FG != null) {
 	  this.Images.Add(FG);
 	  Pos = BR.BaseStream.Position;
-	  this.SetProgress(Pos, MaxPos);
-	  this.lblScanProgress.Text = String.Format(I18N.GetText("ImageScan"), ++ImageCount);
+	  this.SetProgress(Pos + 1, MaxPos);
+	  this.lblScanProgress.Text = String.Format(I18N.GetText("ImageScanProgress"), ++ImageCount);
 	}
 	else {
 	  BR.BaseStream.Seek(++Pos, SeekOrigin.Begin);
 	  if (Pos == MaxPos || (Pos % 0x100) == 0)
-	    this.SetProgress(Pos, MaxPos);
+	    this.SetProgress(Pos + 1, MaxPos);
 	}
       }
     }
@@ -121,14 +262,13 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
 
     private void ScanItemData(BinaryReader BR) {
       this.lblScanProgress.Text = I18N.GetText("ItemCheck");
-      this.prbScanProgress.Value = 0;
+      this.SetProgress(0, 1);
       if (BR.BaseStream.Length < 0xc00 || (BR.BaseStream.Length % 0xc00) != 0)
 	return;
       BR.BaseStream.Seek(0, SeekOrigin.Begin);
     long ItemCount = BR.BaseStream.Length / 0xc00;
+      this.lblScanProgress.Text = I18N.GetText("ItemLoad");
       for (long i = 0; i < ItemCount; ++i) {
-	this.SetProgress(i + 1, ItemCount);
-	this.lblScanProgress.Text = String.Format(I18N.GetText("ItemLoad"), i, ItemCount);
       byte[] ItemData  = BR.ReadBytes(0xc00);
 	for (int j = 0; j < 0xc00; ++j)
 	  ItemData[j] = (byte) ((ItemData[j] << 3) | (ItemData[j] >> 5));
@@ -146,17 +286,48 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
 	}
 	this.Items.Add(new FFXIItem(i + 1, ItemData, ItemIcon));
 	this.Images.Add(ItemIcon);
+	if (FileScanDialog.ShowProgressDetails)
+	  this.lblScanProgress.Text = String.Format(I18N.GetText("ItemLoadProgress"), i + 1, ItemCount);
+	this.SetProgress(i + 1, ItemCount);
       }
-      this.lblScanProgress.Text = String.Format(I18N.GetText("ItemLoad"), ItemCount, ItemCount);
     }
+
+    #endregion
+
+    #region Status Effects
+
+    // Coming Soon
 
     #endregion
 
     #region String Tables
 
+    private void ScanSimpleStringFile(BinaryReader BR) {
+      this.lblScanProgress.Text = I18N.GetText("StringCheck");
+      this.SetProgress(0, 1);
+      BR.BaseStream.Seek(0, SeekOrigin.Begin);
+      if ((BR.BaseStream.Length % 0x40) != 0)
+	return;
+    long EntryCount = BR.BaseStream.Length / 0x40;
+    Encoding E = new FFXIEncoding();
+      this.lblScanProgress.Text = I18N.GetText("StringLoad");
+      for (int i = 0; i < EntryCount; ++i) {
+      uint EntryIndex = BR.ReadUInt32();
+      byte[] EntryBytes = BR.ReadBytes(0x3c);
+	if (EntryIndex != i || EntryBytes[0x3b] != 0xff) {
+	  this.StringTableEntries.Clear();
+	  return;
+	}
+	this.StringTableEntries.Add(E.GetString(EntryBytes, 0, 0x3b).TrimEnd('\0'));
+	if (FileScanDialog.ShowProgressDetails)
+	  this.lblScanProgress.Text = String.Format(I18N.GetText("StringLoadProgress"), i + 1, EntryCount);
+	this.SetProgress(i + 1, EntryCount);
+      }
+    }
+
     private void ScanXIStringFile(BinaryReader BR) {
       this.lblScanProgress.Text = I18N.GetText("StringCheck");
-      this.prbScanProgress.Value = 0;
+      this.SetProgress(0, 1);
       BR.BaseStream.Seek(0, SeekOrigin.Begin);
       if (BR.BaseStream.Length < 0x38)
 	return;
@@ -179,7 +350,7 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
       BR.ReadUInt32(); // Unknown
       if (EntryBytes != EntryCount * 12 || FileSize != 0x38 + EntryBytes + DataBytes)
 	return;
-      this.lblScanProgress.Text = I18N.GetText("StringScan");
+      this.lblScanProgress.Text = I18N.GetText("StringLoad");
       for (int i = 0; i < EntryCount; ++i) {
       uint  Offset = BR.ReadUInt32();
       short Size = BR.ReadInt16();
@@ -194,47 +365,9 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
 	}
 	else
 	  this.StringTableEntries.Add(I18N.GetText("InvalidEntry"));
-	this.SetProgress(i, EntryCount);
-      }
-    }
-
-    #endregion
-
-    #region Dialog Files
-
-    private void ScanDialogFile(BinaryReader BR) {
-      this.lblScanProgress.Text = I18N.GetText("DialogCheck");
-      this.prbScanProgress.Value = 0;
-      BR.BaseStream.Seek(0, SeekOrigin.Begin);
-      if (BR.BaseStream.Length < 4)
-	return;
-    uint FileSizeMaybe = BR.ReadUInt32();
-      if (FileSizeMaybe != (0x10000000 + BR.BaseStream.Length - 4))
-	return;
-    uint FirstTextPos = (BR.ReadUInt32() ^ 0x80808080);
-      if ((FirstTextPos % 4) != 0 || FirstTextPos > BR.BaseStream.Length || FirstTextPos < 8)
-	return;
-      this.lblScanProgress.Text = I18N.GetText("DialogScan");
-    Encoding E = new FFXIEncoding();
-    uint EntryCount = FirstTextPos / 4;
-      for (int i = 0; i < EntryCount; ++i) {
-	BR.BaseStream.Seek(4 + 4 * i, SeekOrigin.Begin);
-      long Offset = (BR.ReadUInt32() ^ 0x80808080);
-      long NextOffset = (((i + 1) == EntryCount) ? BR.BaseStream.Length : (BR.ReadUInt32() ^ 0x80808080));
-	if (NextOffset < Offset || NextOffset > (Offset + 1024)) { // Sanity check - the 1024 is arbitrary
-	  this.StringTableEntries.Clear();
-	  return;
-	}
-	this.SetProgress(i, EntryCount);
-	if (NextOffset == Offset) {
-	  this.StringTableEntries.Add(String.Empty);
-	  continue;
-	}
-	BR.BaseStream.Seek(4 + Offset, SeekOrigin.Begin);
-      byte[] TextBytes = BR.ReadBytes((int) (NextOffset - Offset));
-	for (int j = 0; j < TextBytes.Length; ++j)
-	  TextBytes[j] ^= 0x80;
-	this.StringTableEntries.Add(E.GetString(TextBytes));
+	if (FileScanDialog.ShowProgressDetails)
+	  this.lblScanProgress.Text = String.Format(I18N.GetText("StringLoadProgress"), i + 1, EntryCount);
+	this.SetProgress(i + 1, EntryCount);
       }
     }
 
@@ -331,21 +464,25 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
 
     #endregion
 
-#if ThreadBased
-
+    private bool   Scanning   = false;
     private Thread ScanThread = null;
 
     private void FileScanDialog_Activated(object sender, System.EventArgs e) {
       lock (this) {
-	if (this.ScanThread != null)
+	if (this.Scanning)
 	  return;
-	this.ScanThread = new Thread(new ThreadStart(this.ScanFile));
-	this.ScanThread.Start();
+	this.Scanning = true;
+	if (FileScanDialog.AllowAbort) {
+	  this.ScanThread = new Thread(new ThreadStart(this.ScanFile));
+	  this.ScanThread.Start();
+	}
+	else
+	  this.ScanFile();
       }
     }
 
     private void FileScanDialog_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
-      if (this.ScanThread != null) {
+      if (FileScanDialog.AllowAbort && this.ScanThread != null) {
 	this.ScanThread.Abort();
 	this.ScanThread = null;
 	this.StringTableEntries.Clear();
@@ -354,24 +491,6 @@ namespace PlayOnline.Utils.FFXIDataBrowser {
 	this.DialogResult = DialogResult.Abort;
       }
     }
-
-#else
-
-    private bool Scanning = false;
-
-    private void FileScanDialog_Activated(object sender, System.EventArgs e) {
-      lock (this) {
-	if (this.Scanning)
-	  return;
-	this.Scanning = true;
-	this.ScanFile();
-      }
-    }
-
-    private void FileScanDialog_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
-    }
-
-#endif
 
   }
 
