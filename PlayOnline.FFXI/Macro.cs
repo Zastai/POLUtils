9a9de126cb4756540e7e6a90ca34cfd6ecef678e
Tim Van Holder <Zastai@users.noreply.github.com>
2005-04-26 19:34:19 +0000

    Get an Encoding as parameter in several methods, instead of instantiating a new local one. Don't XMLize macro text anymore (i.e. no <autotrans> tags, but they're still supported when reading the XML file). In WriteEncodedString(), handle being passed null.

--- PlayOnline.FFXI/Macro.cs
+++ PlayOnline.FFXI/Macro.cs
@@ -63,10 +63,9 @@ namespace PlayOnline.FFXI {
 
     #region MacroBar Access
 
-    internal static Macro ReadFromMacroBar(BinaryReader BR) {
+    internal static Macro ReadFromMacroBar(BinaryReader BR, Encoding E) {
     Macro M = new Macro();
       if (BR != null) {
-      Encoding E = new FFXIEncoding();
 	BR.ReadInt32(); // Unknown
 	for (int i = 0; i < 6; ++i) { // 6 Lines of text, 61 bytes each, null-terminated shift-jis
 	string Command = "";
@@ -78,8 +77,7 @@ namespace PlayOnline.FFXI {
       return M;
     }
 
-    internal void WriteToMacroBar(BinaryWriter BW) {
-    Encoding E = new FFXIEncoding();
+    internal void WriteToMacroBar(BinaryWriter BW, Encoding E) {
       BW.Write((uint) 0);
       for (int i = 0; i < 6; ++i) // 6 Lines of text, 61 bytes each, nul-terminated shift-jis
 	this.WriteEncodedString(BW, this.Commands_[i], E, 61);
@@ -88,6 +86,8 @@ namespace PlayOnline.FFXI {
 
     private void WriteEncodedString(BinaryWriter BW, string Text, Encoding E, int Bytes) {
     ArrayList OutBytes = new ArrayList(Bytes);
+      if (Text == null)
+	Text = String.Empty;
       OutBytes.AddRange(E.GetBytes(Text));
       while (OutBytes.Count > Bytes)
 	OutBytes.RemoveAt(Bytes);
@@ -104,6 +104,7 @@ namespace PlayOnline.FFXI {
     Macro M = new Macro();
       if (MacroNode.Attributes["name"] != null)
 	M.Name_ = MacroNode.Attributes["name"].InnerText;
+    Encoding E = new FFXIEncoding();
       for (int i = 0; i < 6; ++i) {
       XmlNode CommandNode = MacroNode.SelectSingleNode(String.Format("command[@line = {0}]", i + 1));
 	if (CommandNode != null && CommandNode is XmlElement) {
@@ -111,11 +112,13 @@ namespace PlayOnline.FFXI {
 	  foreach (XmlNode XN in CommandNode.ChildNodes) {
 	    if (XN is XmlText)
 	      CommandText += XN.InnerText;
+	    // Backwards compatibility - to be removed
 	    else if (XN is XmlElement && XN.Name == "autotrans") {
 	    ushort Category = 0; try { XmlAttribute XCat   = XN.Attributes["category"]; Category = ushort.Parse(XCat.InnerText);   } catch {}
 	    byte   Group    = 0; try { XmlAttribute XGroup = XN.Attributes["group"];    Group    =   byte.Parse(XGroup.InnerText); } catch {}
 	    byte   ID       = 0; try { XmlAttribute XID    = XN.Attributes["id"];       ID       =   byte.Parse(XID.InnerText);    } catch {}
-	      CommandText += String.Format("\x00AB{0}/{1}/{2}: {3}\x00BB", Category, Group, ID, AutoTranslator.GetMessage(Group, ID, Category));
+	    byte[] ResourceMarker = new byte[] { 0xFD, (byte) (Category & 0xff), (byte) (Category >> 8), Group, ID, 0xFD };
+	      CommandText += E.GetString(ResourceMarker);
 	    }
 	  }
 	  M.Commands_[i] = CommandText;
@@ -139,53 +142,7 @@ namespace PlayOnline.FFXI {
 	    XLine.InnerText = String.Format("{0}", i + 1);
 	    XCommand.Attributes.Append(XLine);
 	  }
-	string CommandText = this.Commands_[i];
-	int MarkerPos = CommandText.IndexOf('\x00AB', 0);
-	  while (MarkerPos >= 0) {
-	  int EndMarkerPos = CommandText.IndexOf('\x00BB', MarkerPos);
-	  int NextMarkerPos = CommandText.IndexOf('\x00AB', MarkerPos + 1);
-	    if (EndMarkerPos > 0 && (NextMarkerPos < 0 || NextMarkerPos > EndMarkerPos)) { // Parse Contents
-	      if (MarkerPos > 0) // Save preceding text (if any)
-		XCommand.AppendChild(XDoc.CreateTextNode(CommandText.Substring(0, MarkerPos)));
-	    string Marker = CommandText.Substring(MarkerPos + 1, (EndMarkerPos - MarkerPos - 1));
-	      CommandText = CommandText.Substring(EndMarkerPos + 1);
-	    XmlElement XAutoTrans = null;
-	    string[] MarkerAndText = Marker.Split(new char[] { ':' }, 2);
-	      if (MarkerAndText.Length == 2) {
-	      string[] MarkerValues = MarkerAndText[0].Split('/');
-		if (MarkerValues.Length == 3) {
-		XAutoTrans = XDoc.CreateElement("autotrans");
-		  XAutoTrans.InnerText = MarkerAndText[1].Substring(1); // skip space after the ':'
-		  { // add 'category' attribute
-		  XmlAttribute XATCategory = XDoc.CreateAttribute("category");
-		    XATCategory.InnerText = MarkerValues[0];
-		    XAutoTrans.Attributes.Append(XATCategory);
-		  }
-		  { // add 'group' attribute
-		  XmlAttribute XATLang = XDoc.CreateAttribute("group");
-		    XATLang.InnerText = MarkerValues[1];
-		    XAutoTrans.Attributes.Append(XATLang);
-		  }
-		  { // add 'id' attribute
-		  XmlAttribute XATLang = XDoc.CreateAttribute("id");
-		    XATLang.InnerText = MarkerValues[2];
-		    XAutoTrans.Attributes.Append(XATLang);
-		  }
-		}
-	      }
-	      if (XAutoTrans == null) // invalid marker
-		XCommand.AppendChild(XDoc.CreateTextNode(Marker));
-	      else
-		XCommand.AppendChild(XAutoTrans);
-	    }
-	    else { // Not a valid autotrans marker
-	      XCommand.AppendChild(XDoc.CreateTextNode(CommandText.Substring(0, MarkerPos + 1)));
-	      CommandText = CommandText.Substring(MarkerPos + 1);
-	    }
-	    MarkerPos = CommandText.IndexOf('\x00AB', 0);
-	  }
-	  if (CommandText != String.Empty)
-	    XCommand.AppendChild(XDoc.CreateTextNode(CommandText));
+	  XCommand.AppendChild(XDoc.CreateTextNode(this.Commands_[i]));
 	  XMacro.AppendChild(XCommand);
 	}
       }
