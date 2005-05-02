1b07e89c0a1aee3194e7e3a30b29f169bf760a5a
Tim Van Holder <Zastai@users.noreply.github.com>
2005-05-02 16:18:21 +0000

    New file.  Assembly attributes.

---
+++ PlayOnline.Utils.FFXIItemComparison/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyTitle        ("PlayOnline.Utils.FFXIItemComparison")]
+[assembly: AssemblyDescription  ("Components for comparing exported FFXI item data")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany      ("")]
+[assembly: AssemblyProduct      ("POLUtils")]
+[assembly: AssemblyCopyright    ("Copyright 2005 Tim Van Holder")]
+[assembly: AssemblyTrademark    ("PlayOnline, Final Fantasy and Vanadiel are registered trademarks of Square Enix Co., Ltd.")]
+[assembly: AssemblyCulture      ("")]		
+
+[assembly: AssemblyVersion      ("0.6.0.0")]
+
+[assembly: AssemblyDelaySign    (false)]
+[assembly: AssemblyKeyFile      (@"..\..\..\POLUtils.snk")]
+[assembly: AssemblyKeyName      ("")]
