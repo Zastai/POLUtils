e4f42e9bc53038b7a6ad6ecf20c67b7126d733da
Tim Van Holder <Zastai@users.noreply.github.com>
2005-05-28 12:21:56 +0000

    Messages.resx: added BadResID message. FFXIResourceManager.cs: - Added loading of region names. - Adjusted autotrans loading to set EN/JP text on both entries (0x0104xxxx and 0x0202xxxx). - Decide which language to return based on the UI culture and not the selected POL region. - Use a resource string rather than a hardcoded one for "Unknown String Resource".

--- PlayOnline.FFXI/FFXIResourceManager.cs
+++ PlayOnline.FFXI/FFXIResourceManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.IO;
+using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 using PlayOnline.Core;
@@ -53,6 +55,9 @@ namespace PlayOnline.FFXI {
       Application.DoEvents();
       FFXIResourceManager.LoadItemNames("ROM/118/108.DAT", ItemDataLanguage.English, ItemDataType.Weapon);
       Application.DoEvents();
+      // These aren't "real" resource strings as such, but it's convenient to access them this way
+      FFXIResourceManager.LoadStringTable("ROM/97/48.DAT", "ROM/97/30.DAT", 0x0001); // Region Names
+      Application.DoEvents();
       PWD.Close();
       FFXIResourceManager.Initialized = true;
       FFXIResourceManager.Initializing = false;
@@ -61,8 +66,25 @@ namespace PlayOnline.FFXI {
     private static void LoadAutoTranslatorMessages() {
       foreach (AutoTranslator.MessageGroup MG in AutoTranslator.Data) {
 	foreach (AutoTranslator.Message M in MG.Messages) {
-	uint ResID = (uint) ((uint) ((M.Category & 0xff) << 24) + ((M.Category & 0xff00) << 8) + (ushort) (M.ParentGroup << 8) + M.ID);
-	  FFXIResourceManager.ResourceStrings.Add(ResID, new ResourceString(ResID, M.Text, M.Text));
+	uint ResID = (uint) ((M.ParentGroup << 8) + M.ID);
+	  {
+	  ResourceString RS = FFXIResourceManager.ResourceStrings[0x2020000 + ResID] as ResourceString;
+	    if (RS == null)
+	      FFXIResourceManager.ResourceStrings.Add(0x2020000 + ResID, new ResourceString(0x2020000 + ResID, M.Text, M.Text));
+	    else if (M.Category == 0x0202)
+	      RS.English = M.Text;
+	    else
+	      RS.Japanese = M.Text;
+	  }
+	  {
+	  ResourceString RS = FFXIResourceManager.ResourceStrings[0x1040000 + ResID] as ResourceString;
+	    if (RS == null)
+	      FFXIResourceManager.ResourceStrings.Add(0x1040000 + ResID, new ResourceString(0x1040000 + ResID, M.Text, M.Text));
+	    else if (M.Category == 0x0202)
+	      RS.English = M.Text;
+	    else
+	      RS.Japanese = M.Text;
+	  }
 	}
       }
     }
@@ -89,19 +111,98 @@ namespace PlayOnline.FFXI {
       BR.Close();
     }
 
+    // TODO: Factor out XIString file load (in common with databrowser)
+    private static void LoadStringTable(string FileE, string FileJ, ushort Prefix) {
+    Encoding E = new FFXIEncoding();
+      { // Read English version
+      BinaryReader BR = new BinaryReader(new FileStream(Path.Combine(POL.GetApplicationPath(AppID.FFXI), FileE), FileMode.Open, FileAccess.Read));
+	if ((E.GetString(BR.ReadBytes(10)) != "XISTRING".PadRight(10, '\0')) || BR.ReadUInt16() != 2)
+	  return;
+	foreach (byte B in BR.ReadBytes(20)) {
+	  if (B != 0)
+	    return;
+	}
+      uint FileSize = BR.ReadUInt32();
+	if (FileSize != BR.BaseStream.Length)
+	  return;
+      uint EntryCount = BR.ReadUInt32();
+      uint EntryBytes = BR.ReadUInt32();
+      uint DataBytes  = BR.ReadUInt32();
+	BR.ReadUInt32(); // Unknown
+	BR.ReadUInt32(); // Unknown
+	if (EntryBytes != EntryCount * 12 || FileSize != 0x38 + EntryBytes + DataBytes)
+	  return;
+	for (int i = 0; i < EntryCount; ++i) {
+	  BR.BaseStream.Seek(0x38 + (12 * i), SeekOrigin.Begin);
+	uint  Offset = BR.ReadUInt32();
+	short Size = BR.ReadInt16();
+	string Text = String.Empty;
+	  BR.ReadUInt16(); // Unknown (0 or 1; so probably a flag of some sort)
+	  BR.ReadUInt32(); // Unknown
+	  if (Size > 0 && Offset + Size <= DataBytes) {
+	    BR.BaseStream.Seek(0x38 + EntryBytes + Offset, SeekOrigin.Begin);
+	    Text = E.GetString(BR.ReadBytes(Size)).TrimEnd('\0');
+	  }
+	  else
+	    Text = I18N.GetText("InvalidEntry");
+	ResourceString RS = new ResourceString((uint) ((Prefix << 16) + i), Text, Text);
+	  FFXIResourceManager.ResourceStrings.Add(RS.ID, RS);
+	}
+      }
+      { // Read Japanese version
+      BinaryReader BR = new BinaryReader(new FileStream(Path.Combine(POL.GetApplicationPath(AppID.FFXI), FileJ), FileMode.Open, FileAccess.Read));
+	if ((E.GetString(BR.ReadBytes(10)) != "XISTRING".PadRight(10, '\0')) || BR.ReadUInt16() != 2)
+	  return;
+	foreach (byte B in BR.ReadBytes(20)) {
+	  if (B != 0)
+	    return;
+	}
+      uint FileSize = BR.ReadUInt32();
+	if (FileSize != BR.BaseStream.Length)
+	  return;
+      uint EntryCount = BR.ReadUInt32();
+      uint EntryBytes = BR.ReadUInt32();
+      uint DataBytes  = BR.ReadUInt32();
+	BR.ReadUInt32(); // Unknown
+	BR.ReadUInt32(); // Unknown
+	if (EntryBytes != EntryCount * 12 || FileSize != 0x38 + EntryBytes + DataBytes)
+	  return;
+	for (int i = 0; i < EntryCount; ++i) {
+	  BR.BaseStream.Seek(0x38 + (12 * i), SeekOrigin.Begin);
+	uint  Offset = BR.ReadUInt32();
+	short Size = BR.ReadInt16();
+	string Text = String.Empty;
+	  BR.ReadUInt16(); // Unknown (0 or 1; so probably a flag of some sort)
+	  BR.ReadUInt32(); // Unknown
+	  if (Size > 0 && Offset + Size <= DataBytes) {
+	    BR.BaseStream.Seek(0x38 + EntryBytes + Offset, SeekOrigin.Begin);
+	    Text = E.GetString(BR.ReadBytes(Size)).TrimEnd('\0');
+	  }
+	  else
+	    Text = I18N.GetText("InvalidEntry");
+	uint ResID = (uint) ((Prefix << 16) + i);
+	ResourceString RS = FFXIResourceManager.ResourceStrings[ResID] as ResourceString;
+	  if (RS == null)
+	    FFXIResourceManager.ResourceStrings.Add(ResID, new ResourceString(ResID, Text, Text));
+	  else
+	    RS.Japanese = Text;
+	}
+      }
+    }
+
     public static string GetResourceString(uint ResourceID) {
       if (!FFXIResourceManager.Initialized)
 	FFXIResourceManager.Init();
       if (FFXIResourceManager.Initialized) {
       ResourceString RS = FFXIResourceManager.ResourceStrings[ResourceID] as ResourceString;
 	if (RS != null) {
-	  if (POL.SelectedRegion == POL.Region.Japan)
+	  if (Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName == "ja")
 	    return RS.Japanese;
 	  else
 	    return RS.English;
 	}
       }
-      return "Unknown String Resource";
+      return I18N.GetText("BadResID");
     }
 
   }
