d49cfd37d38e521f957261ef4af7b6ff06bfe9fd
Tim Van Holder <Zastai@users.noreply.github.com>
2006-05-12 17:17:02 +0000

    Adjusted AutoTranslator so that message IDs correctly match the string resource references used in the POL encoding. Corrected shorthand support and added support for @C and @Y (in addition to the existing @A and @J support).

--- PlayOnline.FFXI/AutoTranslator.cs
+++ PlayOnline.FFXI/AutoTranslator.cs
@@ -14,17 +14,21 @@ namespace PlayOnline.FFXI {
 
     public class MessageGroup {
 
-      public byte              ParentGroup;
+      public byte              Category; // 2 = English AT Text, 4 = Japanese AT Text With Alternate
+      public byte              Language; // 1 = Japanese, 2 = English
       public byte              ID;
-      public ushort            Category; // 0x0202 = English Message, 0x0104 = Japanese Message, ...
+      public byte              ParentGroup;
       public string            Title;
       public string            Description;
       public MessageCollection Messages;
 
+      public uint ResID { get { return (uint) ((this.Category << 24) + (this.Language << 16) + (this.ParentGroup << 8) + this.ID); } }
+
       internal MessageGroup() {
-	this.ParentGroup = 0;
-	this.ID          = 0;
 	this.Category    = 0;
+	this.Language    = 0;
+	this.ID          = 0;
+	this.ParentGroup = 0;
 	this.Title       = String.Empty;
 	this.Description = String.Empty;
 	this.Messages    = new MessageCollection();
@@ -34,9 +38,10 @@ namespace PlayOnline.FFXI {
 
     public class Message {
 
-      public byte   ParentGroup;
-      public byte   ID;
-      public ushort Category; // 0x0202 = English Text, 0x0104 = Japanese Text
+      public byte Category; // 2 = English AT Text, 4 = Japanese AT Text With Alternate
+      public byte Language; // 1 = Japanese, 2 = English
+      public byte ID;
+      public byte ParentGroup;
 
       public string Text {
 	get { return this.MaybeExpand(ref this.Text_); }
@@ -50,70 +55,28 @@ namespace PlayOnline.FFXI {
       }
       private string AlternateText_;
 
+      public uint ResID { get { return (uint) ((this.Category << 24) + (this.Language << 16) + (this.ParentGroup << 8) + this.ID); } }
+
       internal Message() {
-	this.ParentGroup   = 0;
-	this.ID            = 0;
 	this.Category      = 0;
+	this.Language      = 0;
+	this.ID            = 0;
+	this.ParentGroup   = 0;
 	this.Text          = String.Empty;
 	this.AlternateText = String.Empty;
       }
 
-      private string GetStringTableEntry(string File, byte EntryNum) {
-      FileStream FS = null;
-	try {
-	  FS = new FileStream(Path.Combine(POL.GetApplicationPath(AppID.FFXI), File), FileMode.Open, FileAccess.Read);
-	BinaryReader BR = new BinaryReader(FS, Encoding.ASCII);
-	  // First: Minimal Sanity Check
-	  BR.BaseStream.Seek(0x20, SeekOrigin.Begin);
-	  if (BR.ReadUInt32() != BR.BaseStream.Length)
-	    return null;
-	uint EntryCount = BR.ReadUInt32();
-	uint EntryBytes = BR.ReadUInt32();
-	  if (EntryCount < EntryNum || EntryBytes != EntryCount * 12)
-	    return null;
-	  // Read the offset & size
-	  BR.BaseStream.Seek(0x38 + 12 * EntryNum, SeekOrigin.Begin);
-	uint  Offset = BR.ReadUInt32();
-	short Size = BR.ReadInt16();
-	  if (Size < 0 || Offset + Size > BR.BaseStream.Length)
-	    return null;
-	  // Read the actual string
-	  BR.BaseStream.Seek(0x38 + EntryBytes + Offset, SeekOrigin.Begin);
-	FFXIEncoding E = new FFXIEncoding();
-	  return E.GetString(BR.ReadBytes(Size)).TrimEnd('\0');
-	} catch {
-	  return null;
-	} finally {
-	  if (FS != null) {
-	    FS.Close();
-	    FS.Dispose();
-	  }
-	}
-      }
-
-      private string GetAreaName(byte EntryNum) {
-      string Name = this.GetStringTableEntry(((this.Category == 0x104) ? "Rom/97/56.dat" : "Rom/97/53.dat"), EntryNum);
-	if (Name != null)
-	  return Name;
-	return String.Format("<Place Name #{0}>", EntryNum);
-      }
-
-      private string GetJobName(byte EntryNum) {
-      string Name = this.GetStringTableEntry(((this.Category == 0x104) ? "Rom/97/29.dat" : "Rom/97/55.dat"), EntryNum);
-	if (Name != null)
-	  return Name;
-	return String.Format("<Job Name #{0}>", EntryNum);
-      }
-
       private string MaybeExpand(ref string Text) {
 	// Reference to a string table entry? => return referenced string
-	if (Text != null && Text.Length == 4 && Text[0] == '@') {
+	if (Text != null && Text.Length > 2 && Text.Length <= 6 && Text[0] == '@') {
 	char ReferenceType = Text[1];
 	  try {
-	  byte EntryNumber = byte.Parse(Text.Substring(2), NumberStyles.AllowHexSpecifier);
+	  uint EntryNumber = uint.Parse(Text.Substring(2), NumberStyles.AllowHexSpecifier);
 	    switch (ReferenceType) {
-	      case 'A': Text = this.GetAreaName(EntryNumber); break;
-	      case 'J': Text = this.GetJobName (EntryNumber); break;
+	      case 'A': Text = FFXIResourceManager.GetResourceString(0x00020000U + EntryNumber); break; // Area
+	      case 'C': Text = FFXIResourceManager.GetResourceString(0x00050000U + EntryNumber); break; // Spell
+	      case 'J': Text = FFXIResourceManager.GetResourceString(0x00030000U + EntryNumber); break; // Job
+	      case 'Y': Text = FFXIResourceManager.GetResourceString(0x00040000U + EntryNumber); break; // Ability
 	    }
 	  } catch { }
 	}
@@ -146,7 +109,8 @@ namespace PlayOnline.FFXI {
 
     private static MessageGroup ReadMessageGroup(BinaryReader BR, Encoding E) {
     MessageGroup MG = new MessageGroup();
-      MG.Category    = BR.ReadUInt16();
+      MG.Category    = BR.ReadByte();
+      MG.Language    = BR.ReadByte();
       MG.ID          = BR.ReadByte();
       MG.ParentGroup = BR.ReadByte();
       MG.Title       = E.GetString(BR.ReadBytes(32)).TrimEnd('\0');
@@ -161,11 +125,12 @@ namespace PlayOnline.FFXI {
 
     private static Message ReadMessage(BinaryReader BR, Encoding E) {
     Message M = new Message();
-      M.Category = BR.ReadUInt16();
+      M.Category    = BR.ReadByte();
+      M.Language    = BR.ReadByte();
       M.ParentGroup = BR.ReadByte();
-      M.ID = BR.ReadByte();
-      M.Text = E.GetString(BR.ReadBytes(BR.ReadByte())).TrimEnd('\0');
-      if (M.Category == 0x0104) // Read alternate text
+      M.ID          = BR.ReadByte();
+      M.Text        = E.GetString(BR.ReadBytes(BR.ReadByte())).TrimEnd('\0');
+      if (M.Category == 4)
 	M.AlternateText = E.GetString(BR.ReadBytes(BR.ReadByte())).TrimEnd('\0');
       return M;
     }
