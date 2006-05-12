962da6f168f82427c9a1c52d13bf484fb020ef2d
Tim Van Holder <Zastai@users.noreply.github.com>
2006-05-12 17:18:35 +0000

    Added job, spell and ability names as string resources. Added loading of key item names as string resources. Reimplemented all string loading to use the FileTypes instead of partially reproducing the loading code (cleaner, but has a performance hit).

--- PlayOnline.FFXI/FFXIResourceManager.cs
+++ PlayOnline.FFXI/FFXIResourceManager.cs
@@ -34,6 +34,19 @@ namespace PlayOnline.FFXI {
     private static SortedList   ResourceStrings = new SortedList();
     private static FFXIEncoding E               = null;
 
+    public static string GetResourceString(uint ResourceID) {
+      if (!FFXIResourceManager.Initialized && !FFXIResourceManager.Initializing)
+	FFXIResourceManager.Init();
+    ResourceString RS = FFXIResourceManager.ResourceStrings[ResourceID] as ResourceString;
+      if (RS != null) {
+	if (Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName == "ja")
+	  return RS.Japanese;
+	else
+	  return RS.English;
+      }
+      return I18N.GetText("BadResID");
+    }
+
     public static void Init() {
       lock (FFXIResourceManager.ResourceStrings) {
 	if (FFXIResourceManager.Initializing || FFXIResourceManager.Initialized)
@@ -45,162 +58,177 @@ namespace PlayOnline.FFXI {
     PleaseWaitDialog PWD = new PleaseWaitDialog(I18N.GetText("PWD:StringResources"));
       PWD.StartPosition = FormStartPosition.CenterScreen;
       PWD.Show();
-      Application.DoEvents();
-      FFXIResourceManager.LoadAutoTranslatorMessages();
-      Application.DoEvents();
-      FFXIResourceManager.LoadItemNames("ROM/118/109.DAT");
-      Application.DoEvents();
-      FFXIResourceManager.LoadItemNames("ROM/118/106.DAT");
-      Application.DoEvents();
-      FFXIResourceManager.LoadItemNames("ROM/118/107.DAT");
-      Application.DoEvents();
-      FFXIResourceManager.LoadItemNames("ROM/118/110.DAT");
-      Application.DoEvents();
-      FFXIResourceManager.LoadItemNames("ROM/118/108.DAT");
-      Application.DoEvents();
-      // These aren't "real" resource strings as such, but it's convenient to access them this way
-      FFXIResourceManager.LoadStringTable("ROM/97/48.DAT", "ROM/97/30.DAT", 0x0001); // Region Names
-      FFXIResourceManager.LoadStringTable("ROM/97/53.DAT", "ROM/97/56.DAT", 0x0002); // Area   Names
-      Application.DoEvents();
+      try {
+	Application.DoEvents();
+	FFXIResourceManager.LoadItemNames("ROM/118/109.DAT");
+	Application.DoEvents();
+	FFXIResourceManager.LoadItemNames("ROM/118/106.DAT");
+	Application.DoEvents();
+	FFXIResourceManager.LoadItemNames("ROM/118/107.DAT");
+	Application.DoEvents();
+	FFXIResourceManager.LoadItemNames("ROM/118/110.DAT");
+	Application.DoEvents();
+	FFXIResourceManager.LoadItemNames("ROM/118/108.DAT");
+	Application.DoEvents();
+	FFXIResourceManager.LoadKeyItemNames("ROM/118/115.DAT", "ROM/118/113.DAT");
+	Application.DoEvents();
+	// These aren't "real" resource strings as such, but it's convenient to access them this way
+	FFXIResourceManager.LoadStringTable ("ROM/97/48.DAT",  "ROM/97/30.DAT",  0x0001); // Region Names
+	Application.DoEvents();
+	FFXIResourceManager.LoadStringTable ("ROM/97/53.DAT",  "ROM/97/56.DAT",  0x0002); // Area Names
+	Application.DoEvents();
+	FFXIResourceManager.LoadStringTable ("ROM/97/55.DAT",  "ROM/97/29.DAT",  0x0003); // Job Names
+	Application.DoEvents();
+	FFXIResourceManager.LoadAbilityNames("ROM/119/55.DAT", "ROM/0/10.DAT", 0x0004); // Ability Names
+	Application.DoEvents();
+	FFXIResourceManager.LoadSpellNames  ("ROM/119/56.DAT", 0x0005);
+	Application.DoEvents();
+	// Needs to be at the end because it references some of the other tables
+	FFXIResourceManager.LoadAutoTranslatorMessages();
+	Application.DoEvents();
+      } catch { /* FIXME: Any action needed? */ }
       PWD.Close();
       FFXIResourceManager.Initialized = true;
       FFXIResourceManager.Initializing = false;
-    }
-
-    private static void LoadAutoTranslatorMessages() {
-      foreach (AutoTranslator.MessageGroup MG in AutoTranslator.Data) {
-	foreach (AutoTranslator.Message M in MG.Messages) {
-	uint ResID = (uint) ((M.ParentGroup << 8) + M.ID);
-	  if (M.Category == 0x0202)
-	    FFXIResourceManager.ResourceStrings.Add(0x2020000 + ResID, new ResourceString(0x2020000 + ResID, M.Text, M.Text));
-	  else
-	    FFXIResourceManager.ResourceStrings.Add(0x1040000 + ResID, new ResourceString(0x1040000 + ResID, M.Text, M.Text));
-	}
-      }
-    }
-
-    private static void LoadItemNames(string File) {
-    BinaryReader BR = new BinaryReader(new FileStream(Path.Combine(POL.GetApplicationPath(AppID.FFXI), File), FileMode.Open, FileAccess.Read));
-    Item.Language L;
-    Item.Type T;
-      Item.DeduceLanguageAndType(BR, out L, out T);
-      while (BR.BaseStream.Position < BR.BaseStream.Length) {
-      byte[] ItemData = BR.ReadBytes(0x200);
-	FFXIEncryption.Rotate(ItemData, 5);
-      BinaryReader IBR = new BinaryReader(new MemoryStream(ItemData, false));
-      uint ResID = 0x07020000U + IBR.ReadUInt32();
-	switch (T) {
-	  case Item.Type.Armor:  IBR.BaseStream.Seek(0x16, SeekOrigin.Current); break;
-	  case Item.Type.Object: IBR.BaseStream.Seek(0x0A, SeekOrigin.Current); break;
-	  case Item.Type.Weapon: IBR.BaseStream.Seek(0x1C, SeekOrigin.Current); break;
-	}
-      string JP = E.GetString(IBR.ReadBytes(22)).TrimEnd('\0');
-      string EN = E.GetString(IBR.ReadBytes(22)).TrimEnd('\0');
-	IBR.Close();
-	FFXIResourceManager.ResourceStrings.Add(ResID, new ResourceString(ResID, EN, JP));
-	BR.BaseStream.Seek(0xa00, SeekOrigin.Current); // Skip icon
-      }
-      BR.Close();
-    }
-
-    // TODO: Factor out XIString file load (in common with databrowser)
-    private static void LoadStringTable(string FileE, string FileJ, ushort Prefix) {
-    Encoding E = new FFXIEncoding();
-      { // Read English version
-      BinaryReader BR = new BinaryReader(new FileStream(Path.Combine(POL.GetApplicationPath(AppID.FFXI), FileE), FileMode.Open, FileAccess.Read));
-	if ((E.GetString(BR.ReadBytes(10)) != "XISTRING".PadRight(10, '\0')) || BR.ReadUInt16() != 2)
-	  return;
-	foreach (byte B in BR.ReadBytes(20)) {
-	  if (B != 0)
-	    return;
-	}
-      uint FileSize = BR.ReadUInt32();
-	if (FileSize != BR.BaseStream.Length)
-	  return;
-      uint EntryCount = BR.ReadUInt32();
-      uint EntryBytes = BR.ReadUInt32();
-      uint DataBytes  = BR.ReadUInt32();
-	BR.ReadUInt32(); // Unknown
-	BR.ReadUInt32(); // Unknown
-	if (EntryBytes != EntryCount * 12 || FileSize != 0x38 + EntryBytes + DataBytes)
-	  return;
-	for (int i = 0; i < EntryCount; ++i) {
-	  BR.BaseStream.Seek(0x38 + (12 * i), SeekOrigin.Begin);
-	uint  Offset = BR.ReadUInt32();
-	short Size = BR.ReadInt16();
-	string Text = String.Empty;
-	  BR.ReadUInt16(); // Unknown (0 or 1; so probably a flag of some sort)
-	  BR.ReadUInt32(); // Unknown
-	  if (Size > 0 && Offset + Size <= DataBytes) {
-	    BR.BaseStream.Seek(0x38 + EntryBytes + Offset, SeekOrigin.Begin);
-	    Text = E.GetString(BR.ReadBytes(Size)).TrimEnd('\0');
-	  }
-	  else
-	    Text = I18N.GetText("InvalidEntry");
-	ResourceString RS = new ResourceString((uint) ((Prefix << 16) + i), Text, Text);
-	  FFXIResourceManager.ResourceStrings.Add(RS.ID, RS);
-	}
-      }
-      { // Read Japanese version
-      BinaryReader BR = new BinaryReader(new FileStream(Path.Combine(POL.GetApplicationPath(AppID.FFXI), FileJ), FileMode.Open, FileAccess.Read));
-	if ((E.GetString(BR.ReadBytes(10)) != "XISTRING".PadRight(10, '\0')) || BR.ReadUInt16() != 2)
-	  return;
-	foreach (byte B in BR.ReadBytes(20)) {
-	  if (B != 0)
-	    return;
-	}
-      uint FileSize = BR.ReadUInt32();
-	if (FileSize != BR.BaseStream.Length)
-	  return;
-      uint EntryCount = BR.ReadUInt32();
-      uint EntryBytes = BR.ReadUInt32();
-      uint DataBytes  = BR.ReadUInt32();
-	BR.ReadUInt32(); // Unknown
-	BR.ReadUInt32(); // Unknown
-	if (EntryBytes != EntryCount * 12 || FileSize != 0x38 + EntryBytes + DataBytes)
-	  return;
-	for (int i = 0; i < EntryCount; ++i) {
-	  BR.BaseStream.Seek(0x38 + (12 * i), SeekOrigin.Begin);
-	uint  Offset = BR.ReadUInt32();
-	short Size = BR.ReadInt16();
-	string Text = String.Empty;
-	  BR.ReadUInt16(); // Unknown (0 or 1; so probably a flag of some sort)
-	  BR.ReadUInt32(); // Unknown
-	  if (Size > 0 && Offset + Size <= DataBytes) {
-	    BR.BaseStream.Seek(0x38 + EntryBytes + Offset, SeekOrigin.Begin);
-	    Text = E.GetString(BR.ReadBytes(Size)).TrimEnd('\0');
-	  }
-	  else
-	    Text = I18N.GetText("InvalidEntry");
-	uint ResID = (uint) ((Prefix << 16) + i);
-	ResourceString RS = FFXIResourceManager.ResourceStrings[ResID] as ResourceString;
-	  if (RS == null)
-	    FFXIResourceManager.ResourceStrings.Add(ResID, new ResourceString(ResID, Text, Text));
-	  else
-	    RS.Japanese = Text;
-	}
-      }
-    }
-
-    public static string GetResourceString(uint ResourceID) {
-      if (!FFXIResourceManager.Initialized)
-	FFXIResourceManager.Init();
-      if (FFXIResourceManager.Initialized) {
-      ResourceString RS = FFXIResourceManager.ResourceStrings[ResourceID] as ResourceString;
-	if (RS != null) {
-	  if (Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName == "ja")
-	    return RS.Japanese;
-	  else
-	    return RS.English;
-	}
-      }
-      return I18N.GetText("BadResID");
     }
 
     public static bool IsValidResourceID(uint ResourceID) {
       if (!FFXIResourceManager.Initialized)
 	FFXIResourceManager.Init();
       return (FFXIResourceManager.Initialized && (FFXIResourceManager.ResourceStrings[ResourceID] as ResourceString) != null);
+    }
+
+    private static void LoadAutoTranslatorMessages() {
+      foreach (AutoTranslator.MessageGroup MG in AutoTranslator.Data) {
+	foreach (AutoTranslator.Message M in MG.Messages)
+	  FFXIResourceManager.ResourceStrings.Add(M.ResID, new ResourceString(M.ResID, M.Text, M.Text));
+      }
+    }
+
+    private static void LoadAbilityNames(string EFile, string JFile, ushort Prefix) {
+    FileType FT = new FileTypes.AbilityInfo();
+    ThingList Contents = null;
+      Contents = FT.Load(Path.Combine(POL.GetApplicationPath(AppID.FFXI), EFile));
+      if (Contents != null) {
+	foreach (IThing T in Contents) {
+	uint ResID = (uint) ((Prefix << 16) + (ushort) T.GetFieldValue("id"));
+	string Name = T.GetFieldText("name");
+	  FFXIResourceManager.ResourceStrings.Add(ResID, new ResourceString(ResID, Name, Name));
+	}
+	Contents.Clear();
+      }
+      Contents = FT.Load(Path.Combine(POL.GetApplicationPath(AppID.FFXI), EFile));
+      if (Contents != null) {
+	foreach (IThing T in Contents) {
+	uint ResID = (uint) ((Prefix << 16) + (ushort) T.GetFieldValue("id"));
+	string Name = T.GetFieldText("name");
+	ResourceString RS = FFXIResourceManager.ResourceStrings[ResID] as ResourceString;
+	  if (RS == null)
+	    FFXIResourceManager.ResourceStrings.Add(ResID, new ResourceString(ResID, Name, Name));
+	  else
+	    RS.Japanese = Name;
+	}
+	Contents.Clear();
+      }
+    }
+
+    private static void LoadItemNames(string File) {
+    FileType FT = new FileTypes.ItemData();
+    ThingList Contents = null;
+      Contents = FT.Load(Path.Combine(POL.GetApplicationPath(AppID.FFXI), File));
+      if (Contents != null) {
+	foreach (IThing T in Contents) {
+	uint   ID    = (uint) T.GetFieldValue("id");
+	string EName =        T.GetFieldText("english-name");
+	string JName =        T.GetFieldText("japanese-name");
+	  FFXIResourceManager.ResourceStrings.Add(0x07010000U + ID, new ResourceString(0x07010000U + ID, EName, JName));
+	  FFXIResourceManager.ResourceStrings.Add(0x07020000U + ID, new ResourceString(0x07020000U + ID, EName, JName));
+	}
+	Contents.Clear();
+      }
+    }
+
+    private static void LoadKeyItemNames(string EFile, string JFile) {
+    FileType FT = new FileTypes.QuestInfo();
+    ThingList Contents = null;
+      Contents = FT.Load(Path.Combine(POL.GetApplicationPath(AppID.FFXI), EFile));
+      if (Contents != null) {
+	foreach (IThing T in Contents) {
+	  if (T is QuestInfo && T.GetFieldText("category") == "sc_item_") {
+	  uint   ID   = (uint) T.GetFieldValue("id");
+	  string Name = T.GetFieldText("name-2");
+	    FFXIResourceManager.ResourceStrings.Add(0x13010000U + ID, new ResourceString(0x13010000U + ID, Name, Name));
+	    FFXIResourceManager.ResourceStrings.Add(0x13020000U + ID, new ResourceString(0x13020000U + ID, Name, Name));
+	  }
+	}
+	Contents.Clear();
+      }
+      Contents = FT.Load(Path.Combine(POL.GetApplicationPath(AppID.FFXI), JFile));
+      if (Contents != null) {
+	foreach (IThing T in Contents) {
+	  if (T is QuestInfo && T.GetFieldText("category") == "sc_item") {
+	  uint   ID   = (uint) T.GetFieldValue("id");
+	  string Name = T.GetFieldText("name-1");
+	  ResourceString RS = null;
+	    RS = FFXIResourceManager.ResourceStrings[0x13010000U + ID] as ResourceString;
+	    if (RS == null)
+	      FFXIResourceManager.ResourceStrings.Add(0x13010000U + ID, new ResourceString(0x13010000U + ID, Name, Name));
+	    else
+	      RS.Japanese = Name;
+	    RS = FFXIResourceManager.ResourceStrings[0x13020000U + ID] as ResourceString;
+	    if (RS == null)
+	      FFXIResourceManager.ResourceStrings.Add(0x13020000U + ID, new ResourceString(0x13020000U + ID, Name, Name));
+	    else
+	      RS.Japanese = Name;
+	  }
+	}
+	Contents.Clear();
+      }
+    }
+
+    private static void LoadSpellNames(string File, ushort Prefix) {
+    FileType FT = new FileTypes.SpellInfo();
+    ThingList Contents = null;
+      Contents = FT.Load(Path.Combine(POL.GetApplicationPath(AppID.FFXI), File));
+      if (Contents != null) {
+	foreach (IThing T in Contents) {
+	  if (T is SpellInfo) {
+	  uint ID = (ushort) T.GetFieldValue("id");
+	    if (ID != 0) {
+	      ID += (uint) (Prefix << 16);
+	      FFXIResourceManager.ResourceStrings.Add(ID, new ResourceString(ID, T.GetFieldText("english-name"), T.GetFieldText("japanese-name")));
+	    }
+	  }
+	}
+	Contents.Clear();
+      }
+    }
+
+    private static void LoadStringTable(string EFile, string JFile, ushort Prefix) {
+    FileType FT = new FileTypes.XIStringTable();
+    ThingList Contents = null;
+      Contents = FT.Load(Path.Combine(POL.GetApplicationPath(AppID.FFXI), EFile));
+      if (Contents != null) {
+	foreach (IThing T in Contents) {
+	uint ResID = (uint) ((Prefix << 16) + (uint) T.GetFieldValue("index"));
+	string Text = T.GetFieldText("text");
+	  FFXIResourceManager.ResourceStrings.Add(ResID, new ResourceString(ResID, Text, Text));
+	}
+	Contents.Clear();
+      }
+      Contents = FT.Load(Path.Combine(POL.GetApplicationPath(AppID.FFXI), EFile));
+      if (Contents != null) {
+	foreach (IThing T in Contents) {
+	uint ResID = (uint) ((Prefix << 16) + (uint) T.GetFieldValue("index"));
+	string Text = T.GetFieldText("text");
+	ResourceString RS = FFXIResourceManager.ResourceStrings[ResID] as ResourceString;
+	  if (RS == null)
+	    FFXIResourceManager.ResourceStrings.Add(ResID, new ResourceString(ResID, Text, Text));
+	  else
+	    RS.Japanese = Text;
+	}
+	Contents.Clear();
+      }
     }
 
   }
