91f4e3ad629b8c4fcce6647ef07ec79159fc18cf
Tim Van Holder <Zastai@users.noreply.github.com>
2006-06-23 19:11:49 +0000

    Made the functions IList<byte>-based rather than byte[]-based. Adjusted ReadEncodedString() to also handle cases where the encoded string is only 1 byte (but the second NUL is still needed to do the decode properly).

--- PlayOnline.FFXI/FFXIEncryption.cs
+++ PlayOnline.FFXI/FFXIEncryption.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -13,14 +14,14 @@ namespace PlayOnline.FFXI {
       return (byte) ((B >> ShiftSize) | (B << (8 - ShiftSize)));
     }
 
-    public static bool Rotate(byte[] Data, byte ShiftSize) {
-      return FFXIEncryption.Rotate(Data, 0L, Data.Length, ShiftSize);
+    public static bool Rotate(IList<byte> Data, byte ShiftSize) {
+      return FFXIEncryption.Rotate(Data, 0, Data.Count, ShiftSize);
     }
 
-    public static bool Rotate(byte[] Data, long Offset, long Size, byte ShiftSize) {
+    public static bool Rotate(IList<byte> Data, int Offset, int Size, byte ShiftSize) {
       if (ShiftSize < 1 || ShiftSize > 8)
 	return false;
-      for (long i = 0; i < Size; ++i)
+      for (int i = 0; i < Size; ++i)
 	Data[Offset + i] = FFXIEncryption.Rotate(Data[Offset + i], ShiftSize);
       return true;
     }
@@ -35,7 +36,7 @@ namespace PlayOnline.FFXI {
       return Count;
     }
 
-    private static byte GetTextShiftSize(byte[] Data, long Offset, long Size) {
+    private static byte GetTextShiftSize(IList<byte> Data, int Offset, int Size) {
       if (Size < 2)
 	return 0;
       if (Data[Offset + 0] == 0 && Data[Offset + 1] == 0)
@@ -53,7 +54,7 @@ namespace PlayOnline.FFXI {
       return 0;
     }
 
-    private static byte GetDataShiftSize(byte[] Data, long Offset, long Size) {
+    private static byte GetDataShiftSize(IList<byte> Data, int Offset, int Size) {
       if (Size < 13)
 	return 0;
       // This is the heuristic that ffxitool uses to determine the shift size - it makes absolutely no
@@ -71,32 +72,28 @@ namespace PlayOnline.FFXI {
       return 0;
     }
 
-    public static bool DecodeTextBlock(byte[] Data) {
-      return FFXIEncryption.DecodeTextBlock(Data, 0L, Data.LongLength);
+    public static bool DecodeTextBlock(IList<byte> Data) {
+      return FFXIEncryption.DecodeTextBlock(Data, 0, Data.Count);
     }
 
-    public static bool DecodeTextBlock(byte[] Data, long Offset, long Size) {
+    public static bool DecodeTextBlock(IList<byte> Data, int Offset, int Size) {
       return FFXIEncryption.Rotate(Data, Offset, Size, FFXIEncryption.GetTextShiftSize(Data, Offset, Size));
     }
 
-    public static bool DecodeDataBlock(byte[] Data) {
-      return FFXIEncryption.DecodeDataBlock(Data, 0L, Data.LongLength);
+    public static bool DecodeDataBlock(IList<byte> Data) {
+      return FFXIEncryption.DecodeDataBlock(Data, 0, Data.Count);
     }
 
-    public static bool DecodeDataBlock(byte[] Data, long Offset, long Size) {
+    public static bool DecodeDataBlock(IList<byte> Data, int Offset, int Size) {
       return FFXIEncryption.Rotate(Data, Offset, Size, FFXIEncryption.GetDataShiftSize(Data, Offset, Size));
     }
 
     public static string ReadEncodedString(BinaryReader BR, Encoding E) {
-    ArrayList LineBytes = new ArrayList();
-    byte B = BR.ReadByte();
-      while (B != 0) {
-	LineBytes.Add(B);
-	B = BR.ReadByte();
-      }
-    byte[] EncodedText = (byte[]) LineBytes.ToArray(typeof(byte));
-      FFXIEncryption.DecodeTextBlock(EncodedText);
-      return E.GetString(EncodedText);
+    List<byte> LineBytes = new List<byte>();
+      // It's NUL-terminated, BUT we need at least two bytes to determine the shift size - for example, a single space becomes "10 00".
+      do { LineBytes.Add(BR.ReadByte()); } while (LineBytes.Count < 2 || (byte) LineBytes[LineBytes.Count - 1] != 0);
+      FFXIEncryption.DecodeTextBlock(LineBytes);
+      return E.GetString(LineBytes.ToArray()).TrimEnd('\0');
     }
 
   }
