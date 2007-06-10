751d5b62c9fb47d0b4d04b30fe2c32525d1accc3
Tim Van Holder <Zastai@users.noreply.github.com>
2007-06-10 15:23:19 +0000

    Improved file format support - entries can contain multiple strings, and up to 5 strings will now be loaded (needed for the Status Names DATs).

--- PlayOnline.FFXI/IThings/DMSGStringTableEntry2.cs
+++ PlayOnline.FFXI/IThings/DMSGStringTableEntry2.cs
@@ -17,7 +17,26 @@ namespace PlayOnline.FFXI.Things {
     }
 
     public override string ToString() {
-      return this.Text_;
+    string Text = String.Empty;
+      if (this.String1_ != null)
+	Text += this.String1_;
+      if (this.String2_ != null) {
+	Text += " / ";
+	Text += this.String2_;
+	if (this.String3_ != null) {
+	  Text += " / ";
+	  Text += this.String3_;
+	  if (this.String4_ != null) {
+	    Text += " / ";
+	    Text += this.String4_;
+	    if (this.String5_ != null) {
+	      Text += " / ";
+	      Text += this.String5_;
+	    }
+	  }
+	}
+      }
+      return Text;
     }
 
     public override List<PropertyPages.IThing> GetPropertyPages() {
@@ -30,7 +49,13 @@ namespace PlayOnline.FFXI.Things {
       get {
 	return new List<string>(new string[] {
 	  "index",
-	  "text",
+	  "string-1",
+	  "string-2",
+	  "string-3",
+	  "string-4",
+	  "string-5",
+	  // 5 is an arbitrary limit; so far I've only seen tables with 1 or 2 strings per entry
+	  "string-count"
 	});
       }
     }
@@ -42,13 +67,23 @@ namespace PlayOnline.FFXI.Things {
     #region Data Fields
 
     private uint?  Index_;
-    private string Text_;
+    private string String1_;
+    private string String2_;
+    private string String3_;
+    private string String4_;
+    private string String5_;
+    private uint?  StringCount_;
     
     #endregion
 
     public override void Clear() {
-      this.Index_ = null;
-      this.Text_  = null;
+      this.Index_       = null;
+      this.String1_     = null;
+      this.String2_     = null;
+      this.String3_     = null;
+      this.String4_     = null;
+      this.String5_     = null;
+      this.StringCount_ = null;
     }
 
     #endregion
@@ -57,32 +92,52 @@ namespace PlayOnline.FFXI.Things {
 
     public override bool HasField(string Field) {
       switch (Field) {
-	case "index": return this.Index_.HasValue;
-	case "text":  return (this.Text_ != null);
-	default:      return false;
+	case "index":        return this.Index_.HasValue;
+	case "string-1":     return (this.String1_ != null);
+	case "string-2":     return (this.String2_ != null);
+	case "string-3":     return (this.String3_ != null);
+	case "string-4":     return (this.String4_ != null);
+	case "string-5":     return (this.String5_ != null);
+	case "string-count": return this.StringCount_.HasValue;
+	default:             return false;
       }
     }
 
     public override string GetFieldText(string Field) {
       switch (Field) {
-	case "index": return (!this.Index_.HasValue ? String.Empty : String.Format("{0:00000}", this.Index_.Value));
-	case "text":  return this.Text_;
-	default:      return null;
+	case "index":        return (!this.Index_.HasValue ? String.Empty : String.Format("{0:00000}", this.Index_.Value));
+	case "string-1":     return ((this.String1_ == null) ? String.Empty : this.String1_);
+	case "string-2":     return ((this.String2_ == null) ? String.Empty : this.String2_);
+	case "string-3":     return ((this.String3_ == null) ? String.Empty : this.String3_);
+	case "string-4":     return ((this.String4_ == null) ? String.Empty : this.String4_);
+	case "string-5":     return ((this.String5_ == null) ? String.Empty : this.String5_);
+	case "string-count": return (!this.StringCount_.HasValue ? String.Empty : this.StringCount_.ToString());
+	default:             return null;
       }
     }
 
     public override object GetFieldValue(string Field) {
       switch (Field) {
-	case "index": return (!this.Index_.HasValue ? null : (object) this.Index_.Value);
-	case "text":  return this.Text_;
-	default:      return null;
+	case "index":        return (!this.Index_.HasValue ? null : (object) this.Index_.Value);
+	case "string-1":     return this.String1_;
+	case "string-2":     return this.String2_;
+	case "string-3":     return this.String3_;
+	case "string-4":     return this.String4_;
+	case "string-5":     return this.String5_;
+	case "string-count": return (!this.StringCount_.HasValue ? null : (object) this.StringCount_.Value);
+	default:             return null;
       }
     }
 
     protected override void LoadField(string Field, System.Xml.XmlElement Node) {
       switch (Field) {
-	case "index": this.Index_ = (uint) this.LoadUnsignedIntegerField(Node); break;
-	case "text":  this.Text_  =        this.LoadTextField           (Node); break;
+	case "index":        this.Index_       = (uint) this.LoadUnsignedIntegerField(Node); break;
+	case "string-1":     this.String1_     = this.LoadTextField(Node); break;
+	case "string-2":     this.String2_     = this.LoadTextField(Node); break;
+	case "string-3":     this.String3_     = this.LoadTextField(Node); break;
+	case "string-4":     this.String4_     = this.LoadTextField(Node); break;
+	case "string-5":     this.String5_     = this.LoadTextField(Node); break;
+	case "string-count": this.StringCount_ = (uint) this.LoadUnsignedIntegerField(Node); break;
       }
     }
 
@@ -99,17 +154,47 @@ namespace PlayOnline.FFXI.Things {
       this.Index_ = Index;
       BR.BaseStream.Position = HeaderBytes + 0x8 * Index.Value;
       try {
-      uint Offset = ~BR.ReadUInt32() + 40;
-      uint Length = ~BR.ReadUInt32() - 40;
+      uint Offset = ~BR.ReadUInt32();
+      uint Length = ~BR.ReadUInt32();
 	if (Length < 0 || Offset + Length > DataBytes)
 	  return false;
 	BR.BaseStream.Position = HeaderBytes + EntryBytes + Offset;
-      byte[] TextBytes = BR.ReadBytes((int) Length);
-	for (uint i = 0; i < TextBytes.Length; ++i)
-	  TextBytes[i] ^= 0xff;
-	this.Text_ = E.GetString(TextBytes).TrimEnd('\0');
+	this.StringCount_ = ~BR.ReadUInt32();
+      uint[] StringOffsets = new uint[this.StringCount_.Value];
+      uint[] StringFlags   = new uint[this.StringCount_.Value];
+	for (uint i = 0; i < this.StringCount_.Value; ++i) {
+	  StringOffsets[i] = ~BR.ReadUInt32();
+	  StringFlags  [i] = ~BR.ReadUInt32();
+	  if (StringOffsets[i] < 0 || (StringFlags[i] != 0 && StringFlags[i] != 1))
+	    return false;
+	}
+	// Meaning of flags is unclear so far.
+	for (uint i = 0; i < this.StringCount_.Value; ++i) {
+	   // not sure why the offsets are based 28 bytes past the start of the entry, but they are
+	  BR.BaseStream.Position = HeaderBytes + EntryBytes + Offset + 28 + StringOffsets[i];
+	List<byte> TextBytes = new List<byte>();
+	  while (true) {
+	  byte[] FourBytes = BR.ReadBytes(4);
+	    FourBytes[0] ^= 0xff;
+	    FourBytes[1] ^= 0xff;
+	    FourBytes[2] ^= 0xff;
+	    FourBytes[3] ^= 0xff;
+	    TextBytes.AddRange(FourBytes);
+	    if (FourBytes[3] == 0)
+	      break;
+	  }
+	string Text = E.GetString(TextBytes.ToArray()).TrimEnd('\0');
+	  switch (i) {
+	    case 0: this.String1_ = Text; break;
+	    case 1: this.String2_ = Text; break;
+	    case 2: this.String3_ = Text; break;
+	    case 3: this.String4_ = Text; break;
+	    case 4: this.String5_ = Text; break;
+	  }
+	}
 	return true;
       } catch { }
+      this.Clear();
       return false;
     }
 
