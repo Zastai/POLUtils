dfbf0337f65a2e0bae7dbfb67e091d37fc699caa
radecforever <radecforever@gmail.com>
2012-11-22 19:51:41 +0000

    Added mob list handling for 0x013XXXXX zones. Added mob lists for Meeble and MMM zones.

--- MassExtractor/Program.cs
+++ MassExtractor/Program.cs
@@ -236,6 +236,8 @@ namespace MassExtractor {
 	  // Mob Lists
 	  for (ushort i = 0; i < 0x100; ++i)
 	    Program.ExtractFile(6720 + i, String.Format("mob-list-{0:000}.xml", i));
+	  for (ushort i = 0; i < 0x100; ++i)
+	    Program.ExtractFile(67910 + i, String.Format("mob-list2-{0:000}.xml", i));
 	  // String Tables
 	  Program.ExtractFile(55465, "area-names.xml");
 	  Program.ExtractFile(55466, "area-names-search.xml");
--- PlayOnline.FFXI/IThings/MobListEntry.cs
+++ PlayOnline.FFXI/IThings/MobListEntry.cs
@@ -105,7 +105,8 @@ namespace PlayOnline.FFXI.Things {
 	this.Name_ = E.GetString(BR.ReadBytes(0x18)).TrimEnd('\0');
 	this.ID_   = BR.ReadUInt32();
 	// ID seems to be 010 + zone id + mob id (=> there's a hard max of 0xFFF (= 4095) mobs per zone, which seems plenty :))
-	if (this.ID_ != 0 && (this.ID_ & 0xFFF00000) != 0x01000000)
+    // Special 'instanced' zones like MMM or Meebles use 013 + 'zone id' + mob id.
+	if ((this.ID_ != 0 && (this.ID_ & 0xFFF00000) != 0x01000000) && (this.ID_ != 0 && (this.ID_ & 0xFFF00000) != 0x01300000))
 	  return false;
 	return true;
       } catch { return false; }
